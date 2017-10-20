62a697665e8cb1f0f90896ed4d5f399a73d9b36c
tanc1807 <tanmay.colaco@neebal.com>
2017-10-20 21:54:08 +0530

    small bug fixed

--- src/main/java/com/pathasoft/dao/ReportTemplateTestRelRepository.java
+++ src/main/java/com/pathasoft/dao/ReportTemplateTestRelRepository.java
@@ -12,7 +12,7 @@ import com.pathasoft.model.Test;
 public interface ReportTemplateTestRelRepository extends JpaRepository<ReportTemplateTestRel, Long> {
 	
 	
-	@Query("select rttr.test, rttr.reportTemplate from ReportTemplateTestRel rttr join rttr.reportTemplate rt  where rt.reportName = ?1 ")
+	@Query("select rttr.test from ReportTemplateTestRel rttr join rttr.reportTemplate rt  where rt.reportName = ?1 ")
 	List<Test> getTestInReportTemplate(String templateName);
 	
 	
